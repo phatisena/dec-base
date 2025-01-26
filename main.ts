4ba4a0961ad3c3e0d0da6df5011acbc8616647e0
Phatisena <naphatism@hotmail.com>
2025-01-26 17:05:33 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ namespace decbase {
             while (ani > 0) {
                 sti = "" + anmt.charAt(ani % bvl) + sti
                 ani = Math.floor(ani / bvl)
-                if (dvl != null) { if (sti.length >= dvl) { break; } }
+                if (dvl != null && dvl > 0) { if (sti.length >= dvl) { break; } }
             }
         } else {
             sti = anmt.charAt(0)
